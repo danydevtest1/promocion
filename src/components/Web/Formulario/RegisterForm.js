abdb640a4298b886c852c07452b4c6f7d737b2f9
Dany <danycambrano1@gmail.com>
2025-01-24 13:27:32 -0600

    Cambios en el form

--- src/components/Web/Formulario/RegisterForm.js
+++ src/components/Web/Formulario/RegisterForm.js
@@ -11,32 +11,6 @@ export function RegisterForm() {
           <Row className="mb-3">
             <Col xs={6}>
               <h3>
-                <em>Datos personales</em>
-              </h3>
-              <Form.Group className="mb-3" controlId="formGroupEmail">
-                <Form.Control type="text" placeholder="CURP" />
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="formGroupEmail">
-                <Form.Control type="text" placeholder="Nombre completo" />
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="formGroupEmail">
-                <Form.Control type="text" placeholder="Apellidos" />
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="formGroupEmail">
-                <Form.Control
-                  type="text"
-                  placeholder="Teléfono: 934-12-27-344"
-                />
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="formGroupEmail">
-                <Form.Control
-                  type="email"
-                  placeholder="Correo: ejemplo@gru.com"
-                />
-              </Form.Group>
-            </Col>
-            <Col xs={6}>
-              <h3>
                 <em>Escuela de procedencia</em>
               </h3>
               <Form.Group
@@ -44,7 +18,7 @@ export function RegisterForm() {
                 controlId="formGroupEmail"
               >
                 <Form.Select>
-                  <option>Selecciona tu escuela</option>
+                  <option>Escuela</option>
                   <option value="1">EMSAD 32</option>
                   <option value="2">CBTA 265</option>
                   <option value="3">COBATAB 08</option>
@@ -69,40 +43,37 @@ export function RegisterForm() {
                 </Form.Select>
               </Form.Group>
               <Form.Group className="mb-3">
-                <Form.Control
-                  type="email"
-                  placeholder="Localidad"
-                />
+                <Form.Control type="email" placeholder="Localidad" />
               </Form.Group>
               <Form.Group className="mb-3">
-              <Form.Select>
-                <option>Estado</option>
-                <option value="1">Tabasco</option>
-                <option value="2">Chiapas</option>
-                <option value="3">Campeche</option>
-                <option value="3">Yucatan</option>
-                <option value="3">Veracruz</option>
-                <option value="3">Ing. Electromecánica</option>
-                <option value="3">Lic. En Administración</option>
-              </Form.Select>
+                <Form.Select>
+                  <option>Estado</option>
+                  <option value="1">Tabasco</option>
+                  <option value="2">Chiapas</option>
+                  <option value="3">Campeche</option>
+                  <option value="3">Yucatan</option>
+                  <option value="3">Veracruz</option>
+                  <option value="3">Ing. Electromecánica</option>
+                  <option value="3">Lic. En Administración</option>
+                </Form.Select>
               </Form.Group>
-             
-              <Form.Select>
-                <option>Municipio</option>
-                <option value="1">Tabasco</option>
-                <option value="2">Chiapas</option>
-                <option value="3">Campeche</option>
-                <option value="3">Yucatan</option>
-                <option value="3">Veracruz</option>
-                <option value="3">Ing. Electromecánica</option>
-                <option value="3">Lic. En Administración</option>
-              </Form.Select>
-            
+              <Form.Group className="mb-3">
+                <Form.Select>
+                  <option>Municipio</option>
+                  <option value="1">Tabasco</option>
+                  <option value="2">Chiapas</option>
+                  <option value="3">Campeche</option>
+                  <option value="3">Yucatan</option>
+                  <option value="3">Veracruz</option>
+                  <option value="3">Ing. Electromecánica</option>
+                  <option value="3">Lic. En Administración</option>
+                </Form.Select>
+              </Form.Group>
               <h3>
                 <em>Carrera</em>
               </h3>
               <Form.Select>
-                <option>Selecciona la carrera</option>
+                <option>Carrera</option>
                 <option value="1">Ing. En Sistemas Computacionales</option>
                 <option value="2">Ing. Bioquímica</option>
                 <option value="3">Ing. Industrial</option>
@@ -111,6 +82,26 @@ export function RegisterForm() {
                 <option value="3">Ing. Electromecánica</option>
                 <option value="3">Lic. En Administración</option>
               </Form.Select>
+            </Col>
+            <Col xs={6}>
+              <h3>
+                <em>Datos personales</em>
+              </h3>
+              <Form.Group className="mb-3" controlId="formGroupEmail">
+                <Form.Control type="text" placeholder="Nombre completo" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formGroupEmail">
+                <Form.Control type="text" placeholder="Apellidos" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formGroupEmail">
+                <Form.Control
+                  type="text"
+                  placeholder="934-12-27-344"
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formGroupEmail">
+                <Form.Control type="email" placeholder="ejemplo@gru.com" />
+              </Form.Group>
             </Col>
             <div className="d-grid gap-2 p-2">
               <Button variant="danger" size="md">
